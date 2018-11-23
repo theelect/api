6b5690c519bced2eec6fae50e5766ceb808d2d38
johnson ejezie <johnsonejezie@gmail.com>
2018-11-23 07:33:17 +0100

    endpoint for lga and wards

--- src/api/routes/index.js
+++ src/api/routes/index.js
@@ -2,12 +2,14 @@ import Auth from './auth';
 import Campaign from './campaign';
 import User from './user';
 import PVC from './pvc';
+import LGA from './lga';
 
 export default (app) => {
   Auth(app);
   Campaign(app);
   User(app);
   PVC(app);
+  LGA(app);
   app.get('/healthcheck', (req, res) => {
     res.json({ message: 'Working' });
   });
